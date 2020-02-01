82c46048d46dec02d00779a9eec833a40303e7a0
dannegm <dannegm@gmail.com>
2020-01-31 22:41:40 -0600

    Player Controller

---
+++ Assets/Source/Controllers/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    enum Directions {
+        Left = -1,
+        Right = 1,
+    }
+
+    public Rigidbody body;
+    public float Speed = 5f;
+    // Start is called before the first frame update
+}
