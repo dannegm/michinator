b846dd39c3b1524f89a40a3b2021a8fa130fac64
unknown <alain101995@gmail.com>
2020-02-02 06:16:43 -0600

    Dialog Box

--- Assets/Source/Controllers/NPCController.cs
+++ Assets/Source/Controllers/NPCController.cs
@@ -4,20 +4,24 @@ using UnityEngine.Events;
 public class NPCController : MonoBehaviour {
 
     public MeshRenderer dialogRender;
+    public SpriteRenderer textBox;
 
 	  void Awake () {
 		  dialogRender.enabled = false;
+      textBox.enabled = false;
 	  }
     
     void OnCollisionEnter (Collision other) {
 		if (other.gameObject.tag == "Player") {
       dialogRender.enabled = true;
+      textBox.enabled = true;
 		}
 	}
 
     void OnCollisionExit (Collision other) {
 		if (other.gameObject.tag == "Player") {
       dialogRender.enabled = false;
+      textBox.enabled = false;
     }
 	}
 
