a68e0cc557c29ff60e0312c11defb1927d96aa22
dannegm <dannegm@gmail.com>
2020-02-02 06:59:47 -0600

    Dialog System

---
+++ Assets/Source/Controllers/DialogController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour {
+    public RectTransform dialog;
+    public Text text;
+
+    public void showDialog (string chat) {
+        text.text = chat;
+        dialog.gameObject.active = true;
+    }
+
+    public void hideDialog () {
+        dialog.gameObject.active = false;
+        text.text = "";
+    }
+}
--- Assets/Source/Controllers/NPCController.cs
+++ Assets/Source/Controllers/NPCController.cs
@@ -2,27 +2,42 @@
 using UnityEngine.Events;
 
 public class NPCController : MonoBehaviour {
-
-    public MeshRenderer dialogRender;
-    public SpriteRenderer textBox;
-
-	  void Awake () {
-		  dialogRender.enabled = false;
-      textBox.enabled = false;
-	  }
-    
-    void OnCollisionEnter (Collision other) {
-		if (other.gameObject.tag == "Player") {
-      dialogRender.enabled = true;
-      textBox.enabled = true;
-		}
-	}
-
-    void OnCollisionExit (Collision other) {
-		if (other.gameObject.tag == "Player") {
-      dialogRender.enabled = false;
-      textBox.enabled = false;
+    enum Direction {
+        LEFT = -1,
+        RIGHT = 1,
     }
-	}
+
+    [Header("Models")]
+    public Transform npcPivot;
+    public Transform playerTransform;
+    public DialogController dialogController;
+
+    [Header("Visuals")]
+    public Vector3 leftDirection;
+    public Vector3 rightDirection;
+ 
+    [Header("Presets")]
+    [TextArea(3, 5)]
+    public string ChatMessage;
+
+    void FixedUpdate () {
+        if (playerTransform.position.x < transform.position.x) {
+            npcPivot.rotation = Quaternion.Euler(leftDirection);
+        } else {
+            npcPivot.rotation = Quaternion.Euler(rightDirection);
+        }
+    }
+    
+    void OnTriggerEnter (Collider other) {
+        if (other.tag == "Player") {
+            dialogController.showDialog(ChatMessage);
+        }
+    }
+
+    void OnTriggerExit (Collider other) {
+        if (other.tag == "Player") {
+            dialogController.hideDialog();
+        }
+    }
 
 }
