7bfd78ed797f67e8040906c37cf79ea61f19a9b5
dannegm <dannegm@gmail.com>
2020-02-02 06:16:25 -0600

    2nd Level

---
+++ Assets/Source/Controllers/LavaController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaController : MonoBehaviour {
+    [Header("Models")]
+    public Lava lavaModel;
+
+    [Header("Pressets")]
+    public float Delay = 1f;
+    float elapsed = 0f;
+    bool canDoDamage = true;
+
+    void FixedUpdate () {
+        elapsed += Time.deltaTime;
+        if (elapsed >= Delay) {
+            elapsed = elapsed % Delay;
+            canDoDamage = true;
+        }
+    }
+    
+    // > Collisions
+	void OnCollisionStay (Collision other) {
+		if (canDoDamage && other.gameObject.tag == "Player") {
+            canDoDamage = false;
+			PlayerController player = other.transform.GetComponent<PlayerController>();
+            player.onDamage(lavaModel.Damage);
+		}
+	}
+}
---
+++ Assets/Source/Models/Lava.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour {
+    [Header("Stats")]
+    public int Damage = 1;
+}
