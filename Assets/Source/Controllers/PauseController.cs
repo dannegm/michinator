a2447e5c5fdf03f31152a12ee3d1836b24ede7cc
dannegm <dannegm@gmail.com>
2020-02-02 11:13:10 -0600

    Improve Pause

---
+++ Assets/Source/Controllers/PauseController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseController : MonoBehaviour {
+    [Header("Models")]
+    public GameObject pauseMenuUI;
+
+    [Header("Events")]
+    public UnityEvent OnPause;
+    public UnityEvent OnResume;
+
+    private bool isPaused;
+
+    void Update () {
+        if (isPaused) {
+            onPause ();
+        } else {
+            onResume ();
+        }
+    }
+
+    public void Pause () {
+        isPaused = true;
+    }
+
+    public void Resume () {
+        isPaused = false;
+    }
+
+    public void switchPause () {
+        isPaused = !isPaused;
+    }
+
+    void onPause () {
+        OnPause.Invoke ();
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    void onResume () {
+        OnResume.Invoke ();
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+    }
+}
