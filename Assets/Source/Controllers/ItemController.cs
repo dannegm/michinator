a6844a610ed84a61d17a0dda2debc5b06131a2d3
dannegm <dannegm@gmail.com>
2020-02-02 08:59:38 -0600

    Cambios Varios

---
+++ Assets/Source/Controllers/ItemController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ItemController : MonoBehaviour {
+    [Header("Models")]
+    public GameObject prefab;
+
+    [Header("Events")]
+    public UnityEvent OnCollect;
+    // > Collisions
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+            OnCollect.Invoke ();
+			Player player = other.GetComponent<Player>();
+            player.ItemsFound += 1;
+            Destroy(prefab);
+		}
+	}
+}
