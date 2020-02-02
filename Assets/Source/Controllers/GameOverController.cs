e1de4f770b079da9548821e7e586ec059bf513f9
dannegm <dannegm@gmail.com>
2020-02-02 11:54:20 -0600

    Release 1

---
+++ Assets/Source/Controllers/GameOverController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameOverController : MonoBehaviour {
+    [Header("Models")]
+    public GameObject gameOverUI;
+
+    [Header("Events")]
+    public UnityEvent OnGameOver;
+    public UnityEvent OnTryAgain;
+
+    public void onGameOver () {
+        OnGameOver.Invoke ();
+        Time.timeScale = 0;
+        gameOverUI.SetActive(true);
+    }
+
+    public void onTryAgain () {
+        OnTryAgain.Invoke ();
+        Time.timeScale = 1;
+        gameOverUI.SetActive(false);
+    }
+}
